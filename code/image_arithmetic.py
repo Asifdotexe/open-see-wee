3c5e3697d78dc030540f7ca1ee5c44ee6c7bfebb
Asif Sayyed <asifdotexe@gmail.com>
2024-07-27 08:39:58 +0530

    #5 adding colored images

--- code/image_arithmetic.py
+++ code/image_arithmetic.py
@@ -9,3 +9,15 @@ image2_resized = cv.resize(image2, (width, height))
 
 sum_image = cv.add(image1, image2_resized)
 cv.imwrite(r'..\image\sum_image.png', sum_image)
+
+# adding color images
+image_color1 = cv.imread(r'..\image\shinchan.png')
+image_color2 = cv.imread(r'..\image\okay.jpg')
+
+height, width, channels = image_color1.shape
+image_color2_resized = cv.resize(image_color2, (width, height))
+sum_image_colored = cv.add(image_color1, image_color2_resized)
+cv.imwrite(r'..\image\sum_image_colored.png', sum_image_colored)
+
+
+
