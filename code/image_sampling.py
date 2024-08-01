fa187c4038f23a7590674ff5bb1852f393061b62
Asif Sayyed <asifdotexe@gmail.com>
2024-08-01 08:23:31 +0530

    #5 image up and down sampling

---
+++ code/image_sampling.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+# RBG
+image = cv.imread(r'..\image\ok.jpg')
+# print(image.shape)
+
+# upsampling
+imageUpSampled = cv.pyrUp(image)
+cv.imwrite(r'..\image\ok_upsampled.jpg', imageUpSampled)
+
+# downsampling
+imageDownSample = cv.pyrDown(image)
+cv.imwrite(r'..\image\ok_downsampled.jpg', imageDownSample)
