e5088e2a4cb552b113243e376eea6dbadd8e19e8
Asif Sayyed <asifdotexe@gmail.com>
2024-08-06 07:40:44 +0530

    #5 performed image bitwise operation on binary image

---
+++ code/image_bitwise_xor.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+image1 = cv.imread(r'..\image\binary_one.png')
+image2 = cv.imread(r'..\image\binary_two.png')
+
+height, width = image1.shape[:2]
+image2_resized = cv.resize(image2, (width, height))
+
+xor_image = cv.bitwise_xor(image1, image2_resized, mask=None)
+cv.imwrite(r'..\image\bitwise_xor_binary.jpg', xor_image)
