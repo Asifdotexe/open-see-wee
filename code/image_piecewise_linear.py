a399e8dcf1d0ad4a1e08450948592b8e187c01d5
Asif Sayyed <asifdotexe@gmail.com>
2024-08-10 08:58:46 +0530

    #9 performed power law transformation

--- code/image_piecewise_linear.py
+++ code/image_piecewise_linear.py
@@ -3,16 +3,16 @@ import numpy as np
 
 image = cv.imread(r'..\image\ok.jpg')
 
-row, col = image.shape
+row, col = image.shape[:2]
 output_image = np.zeros((row, col), dtype='uint8')
 range_min = 80
 range_max = 140
 
 for i in range(row):
     for j in range(col):
-        if image[i,j] > range_min and image[i,j] < range_max:
+        if (image[i,j] > range_min).all() and (image[i,j] < range_max).all():
             output_image[i,j] = 255
         else:
-            output_image[i,j] = image[i-1,j-1]
+            output_image[i,j] = image[i-1,j-1].min()
             
 cv.imwrite(r'..\image\ok_piecewise_linear.jpg', output_image)
