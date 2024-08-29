e032979a618aec077072475279dfdb74d4aef3e2
Asif Sayyed <asifdotexe@gmail.com>
2024-08-29 08:10:23 +0530

    #11 creating histogram

---
+++ code/image_histogram.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv.imread(r'..\image\ok.jpg')
+
+histogram = cv.calcHist([image], [0], None, [256], [0,256])
+plt.plot(histogram)
+plt.xlim([0,256])
+plt.show()
