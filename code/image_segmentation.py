92b0b9dedeb9b3a4e5fcc6b828295b686697f14d
Asif Sayyed <asifdotexe@gmail.com>
2024-09-16 08:44:18 +0530

    #19 performed image segmentation and contouring

---
+++ code/image_segmentation.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+image = cv.imread('../image/luffy.jpg')
+image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# apply binary thresholding
+ret, threshold_image = cv.threshold(image_gray, 150, 200, cv.THRESH_BINARY)
+cv.imwrite('../image/luffy_segmentation_threshold.png', threshold_image)
+# display the threshold image
+# detect contours on the binary image
+contours, hierarchy = cv.findContours(
+    image=threshold_image, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE
+)
+
+image_copy = image.copy()
+output = cv.drawContours(
+    image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), 
+    thickness=2, lineType=cv.LINE_AA
+)
+
+cv.imwrite('../image/delete_karde_bro.png', output)
