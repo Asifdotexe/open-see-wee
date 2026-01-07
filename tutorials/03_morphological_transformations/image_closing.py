f6ae3b1d28e46908a3900d51560373026220804a
Asif Sayyed <asifdotexe@gmail.com>
2026-01-07 11:11:56 +0530

    #29 updated the absolute to relative paths morphological transformation

--- tutorials/03_morphological_transformations/image_closing.py
+++ tutorials/03_morphological_transformations/image_closing.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 import numpy as np
 
-image = cv.imread(r'..\image\binary_one.png')
-image_rick = cv.imread(r'..\image\flag.png')
+image = cv.imread(r'..\..\image\binary_one.png')
+image_rick = cv.imread(r'..\..\image\rickroll.jpg', cv.IMREAD_GRAYSCALE)
 
 kernal= np.ones((3,3), np.uint8)
 close_hand = cv.morphologyEx(image, cv.MORPH_CLOSE ,kernal)
 close_rick = cv.morphologyEx(image_rick, cv.MORPH_CLOSE ,kernal)
 
-cv.imwrite(r'../image/hand_closing.jpg',close_hand)
-cv.imwrite(r'../image/rick_closing.png',close_rick)
+cv.imwrite(r'../../image/hand_closing.jpg',close_hand)
+cv.imwrite(r'../../image/rick_closing.jpg',close_rick)
--- tutorials/03_morphological_transformations/image_morph_gradient.py
+++ tutorials/03_morphological_transformations/image_morph_gradient.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-image = cv.imread('../image/luffy.jpg')
+image = cv.imread('../../image/luffy.jpg')
 kernel = np.ones((3,3), np.uint8)
 
 morph_gradient = cv.morphologyEx(image, cv.MORPH_GRADIENT, kernel)
-cv.imwrite('../image/morph_gradient_luffy.png', morph_gradient)
+cv.imwrite('../../image/morph_gradient_luffy.png', morph_gradient)
